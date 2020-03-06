ad2fefab902b0db26d4c170f661718bc32704d25
ziggybaba1 <seyiadejugbagbe@gmail.com>
2020-03-06 16:27:15 +0100

    Updates

---
+++ precache-manifest.45f993e0c6e5523d6025c16b6f9a6076.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744b85d2dc974eded72e4ed87ebf102f",
+    "url": "/Robo-Friend/index.html"
+  },
+  {
+    "revision": "fd80ccc1d010e758ea07",
+    "url": "/Robo-Friend/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "37372ced06676703cb80",
+    "url": "/Robo-Friend/static/css/main.68f33f12.chunk.css"
+  },
+  {
+    "revision": "fd80ccc1d010e758ea07",
+    "url": "/Robo-Friend/static/js/2.1f772498.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Robo-Friend/static/js/2.1f772498.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37372ced06676703cb80",
+    "url": "/Robo-Friend/static/js/main.0ac483df.chunk.js"
+  },
+  {
+    "revision": "5243c1273496c0993373",
+    "url": "/Robo-Friend/static/js/runtime-main.a5abed31.js"
+  }
+]);
